2ca06d2206782da5a98e7f89574d41bcd4250b16
Alex Tinti <alextinti83@gmail.com>
2017-05-21 10:58:31 +0200

    - folder cannot be added to the favorite list

--- es-app/src/views/gamelist/ISimpleGameListView.cpp
+++ es-app/src/views/gamelist/ISimpleGameListView.cpp
@@ -132,23 +132,26 @@ bool ISimpleGameListView::input(InputConfig* config, Input input)
 			cursor->getSystem()->getIndex()->addToIndex(cursor);
 			onFileChanged(cursor, FILE_METADATA_CHANGED);
 #else
-			const int cursorIndex = getCursorIndex();
-			const int favCount = getFavoritesCount();
-			const bool wasFavorite = cursor->isFavorite();
-			cursor->SetIsFavorite(!wasFavorite);
-			FileChangeType fileChangeType = wasFavorite ? FILE_REMOVED : FILE_ADDED;
-			onFileChanged(cursor, fileChangeType);
+			if (cursor->getType() != FOLDER)
+			{
+				const int cursorIndex = getCursorIndex();
+				const int favCount = getFavoritesCount();
+				const bool wasFavorite = cursor->isFavorite();
+				cursor->SetIsFavorite(!wasFavorite);
+				FileChangeType fileChangeType = wasFavorite ? FILE_REMOVED : FILE_ADDED;
+				onFileChanged(cursor, fileChangeType);
 
-			if ( fileChangeType == FILE_ADDED )
-			{
-				setCursorIndex(cursorIndex + 1); //keep same file selected
-			}
-			else
-			{
-				if (cursorIndex < favCount)
+				if (fileChangeType == FILE_ADDED)
 				{
-					setCursorIndex(cursorIndex);
-				}	
+					setCursorIndex(cursorIndex + 1); //keep same file selected
+				}
+				else
+				{
+					if (cursorIndex < favCount)
+					{
+						setCursorIndex(cursorIndex);
+					}
+				}
 			}
 #endif
 			return true;
