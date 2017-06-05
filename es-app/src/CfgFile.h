a097d22cc29fcce57533fe5ee46874a53e98a2b7
Alex Tinti <alextinti83@gmail.com>
2017-06-05 15:51:41 +0200

    + return entries

--- es-app/src/CfgFile.h
+++ es-app/src/CfgFile.h
@@ -9,51 +9,12 @@ public:
 	CfgEntry(const std::string& key, std::string value) : _key(key), _value(value) { }
 	~CfgEntry() { }
 	std::string GetLine() const;
-	//enum class Type { k_comment, k_bool, k_int, k_real, k_string };
-	//CfgEntry(const std::string& key, bool value)		: _key(key), _type(Type::k_bool),	_bool(value) { }
-	//CfgEntry(const std::string& key, int value)			: _key(key), _type(Type::k_int),	_int(value)	 { }
-	//CfgEntry(const std::string& key, double value)		: _key(key), _type(Type::k_real),	_real(value) { }
-	//
-	//bool IsType(Type type) const { return type == _type; }
-	//
-	//const std::string& GetComment() const 
-	//{ 
-	//	assert(IsType(Type::k_comment));
-	//	return IsType(Type::k_comment) ?_string : "";
-	//}
-	//const std::string& Getkey() const
-	//{
-	//	assert(!IsType(Type::k_comment));
-	//	return !IsType(Type::k_comment) ? _key : "";
-	//}
-	//const std::string& GetString() const
-	//{
-	//	assert(IsType(Type::k_string));
-	//	return IsType(Type::k_string) ? _string : "";
-	//}
-	//const int GetInt() const
-	//{
-	//	assert(IsType(Type::k_int));
-	//	return IsType(Type::k_int) ? _int : 0;
-	//}
-	//const double GetReal() const
-	//{
-	//	assert(IsType(Type::k_real));
-	//	return IsType(Type::k_real) ? _real : 0.0;
-	//}
-	//const bool GetBool() const
-	//{
-	//	assert(IsType(Type::k_bool));
-	//	return IsType(Type::k_bool) ? _bool : false;
-	//}
 
 private:
 	std::string _key;
 	std::string _value;
 	std::string _comment;
 };
-
-
 
 class CfgFile
 {
@@ -69,6 +30,9 @@ public:
 	bool SaveConfigFile(const std::string path);
 	const std::string& GetConfigFilePath() const;
 	std::string GetRawText() const;
+	std::vector<CfgEntry>& GetEntries() { return m_cfgEntries; }
+	const std::vector<CfgEntry>& GetEntries() const { return m_cfgEntries; }
+
 private:
 	void UpdateSignature();
 	std::vector<CfgEntry> m_cfgEntries;
