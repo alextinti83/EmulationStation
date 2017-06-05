fcb8b7824c6dd5260a9929f66b6183d477a80dea
Alex Tinti <alextinti83@gmail.com>
2017-06-05 21:50:12 +0200

    show game config first and system after

--- es-app/src/guis/GuiRetroArchOptions.cpp
+++ es-app/src/guis/GuiRetroArchOptions.cpp
@@ -10,52 +10,51 @@ GuiRetroArchOptions::GuiRetroArchOptions(Window* window, SystemData& system)
 	: GuiOptionWindow(window,"RetroArch Config"), mSystem(system)
 {
 	ComponentListRow row;
-
-	const std::string systemName = strToUpper(mSystem.getName());
-
-
-	// show filtered menu
-	row.elements.clear();
-	std::string title = "EDIT " + systemName + " CONFIGURATION";
-	row.addElement(std::make_shared<TextComponent>(mWindow, title , Font::get(FONT_SIZE_MEDIUM), 0x777777FF), true);
-	row.input_handler = [ this, title ] (InputConfig* config, Input input)
 	{
-		if (config->isMappedTo("a", input) && input.value)
+		const FileData* const file = getGamelist()->getCursor();
+		const uint32_t maxLength = 30; \
+			const std::string origName = file->getName();
+		std::string title = "EDIT ";
+		title += strToUpper(origName.size() > maxLength ? origName.substr(0, maxLength) + "..." : origName);
+		title += " CONFIG";
+		row.elements.clear();
+		row.addElement(std::make_shared<TextComponent>(mWindow, title, Font::get(FONT_SIZE_MEDIUM), 0x777777FF), true);
+		row.input_handler = [ this, title, file ] (InputConfig* config, Input input)
 		{
-			const std::string cfgpath = mSystem.getRetroArchSystemConfigFilepath().generic_string();
-			std::unique_ptr<CfgFile> retroArchConfig(new CfgFile(cfgpath));
-			auto title = "EDIT " + boost::filesystem::path(retroArchConfig->GetConfigFilePath()).filename().generic_string();
-			auto s = new GuiRetroArchConfig(mWindow, title, mSystem, std::move(retroArchConfig));
-			mWindow->pushGui(s);
-			return true;
-		}
-		return false;
-	};
-	addRow(row);
-
-	
-	const FileData* const file = getGamelist()->getCursor();
-	const uint32_t maxLength = 30; \
-	const std::string origName = file->getName();
-	title = "EDIT ";
-	title += strToUpper(origName.size() > maxLength ? origName.substr(0, maxLength) + "...": origName);
-	title += " CONFIG";
-	row.elements.clear();
-	row.addElement(std::make_shared<TextComponent>(mWindow, title, Font::get(FONT_SIZE_MEDIUM), 0x777777FF), true);
-	row.input_handler = [ this, title, file ] (InputConfig* config, Input input)
+			if (config->isMappedTo("a", input) && input.value)
+			{
+				const std::string cfgpath = file->getPath().generic_string() + ".cfg";
+				std::unique_ptr<CfgFile> retroArchConfig(new CfgFile(cfgpath));
+				auto title = "EDIT " + boost::filesystem::path(retroArchConfig->GetConfigFilePath()).filename().generic_string();
+				auto s = new GuiRetroArchConfig(mWindow, title, mSystem, std::move(retroArchConfig));
+				mWindow->pushGui(s);
+				return true;
+			}
+			return false;
+		};
+	}
 	{
-		if (config->isMappedTo("a", input) && input.value)
+		addRow(row);
+
+		const std::string systemName = strToUpper(mSystem.getName());
+		row.elements.clear();
+		std::string title = "EDIT " + systemName + " CONFIGURATION";
+		row.addElement(std::make_shared<TextComponent>(mWindow, title, Font::get(FONT_SIZE_MEDIUM), 0x777777FF), true);
+		row.input_handler = [ this, title ] (InputConfig* config, Input input)
 		{
-			const std::string cfgpath = file->getPath().generic_string() + ".cfg";
-			std::unique_ptr<CfgFile> retroArchConfig(new CfgFile(cfgpath));
-			auto title = "EDIT " + boost::filesystem::path(retroArchConfig->GetConfigFilePath()).filename().generic_string();
-			auto s = new GuiRetroArchConfig(mWindow, title, mSystem, std::move(retroArchConfig));
-			mWindow->pushGui(s);
-			return true;
-		}
-		return false;
-	};
-	addRow(row);
+			if (config->isMappedTo("a", input) && input.value)
+			{
+				const std::string cfgpath = mSystem.getRetroArchSystemConfigFilepath().generic_string();
+				std::unique_ptr<CfgFile> retroArchConfig(new CfgFile(cfgpath));
+				auto title = "EDIT " + boost::filesystem::path(retroArchConfig->GetConfigFilePath()).filename().generic_string();
+				auto s = new GuiRetroArchConfig(mWindow, title, mSystem, std::move(retroArchConfig));
+				mWindow->pushGui(s);
+				return true;
+			}
+			return false;
+		};
+		addRow(row);
+	}
 }
 
 GuiRetroArchOptions::~GuiRetroArchOptions()
