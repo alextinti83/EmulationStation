3ca12b96caa2848e6031e9b189f7669f52f181a1
Alex Tinti <alextinti83@gmail.com>
2017-06-05 14:53:26 +0200

    - inherit from GuiPagedListView

--- es-app/src/guis/GuiImportRetroArchConfig.cpp
+++ es-app/src/guis/GuiImportRetroArchConfig.cpp
@@ -3,17 +3,28 @@
 #include "Window.h"
 #include "views/ViewController.h"
 
-const uint32_t GuiImportRetroArchConfig::k_pageEntryCount(6);
+class GuiImportRetroArchConfigEntry : public GuiPagedListViewEntry
+{
+public:
+	GuiImportRetroArchConfigEntry(boost::filesystem::path path): mPath(path) { }
+	virtual std::string GetText() const override { return mPath.filename().generic_string(); }
+	boost::filesystem::path mPath;
+};
 
 GuiImportRetroArchConfig::GuiImportRetroArchConfig(
 	Window* window,
 	const std::string& title,
 	const boost::filesystem::path& configFolder,
 	CallbackT callback)
-	: GuiOptionWindow(window, title), 
-	m_configFolder(configFolder), 
-	m_onConfigSelected(callback),
-	m_currentPage(0)
+	: GuiPagedListView(window, title, 
+		[ callback ] (GuiPagedListViewEntry* entry)
+		{ 
+			auto cfgEntry = dynamic_cast< GuiImportRetroArchConfigEntry*>( entry );
+			if (cfgEntry)
+			{
+				callback(cfgEntry->mPath);
+			}
+		})
 {
 	if (boost::filesystem::exists(configFolder))
 	{
@@ -24,7 +35,8 @@ GuiImportRetroArchConfig::GuiImportRetroArchConfig(
 		{
 			++count;
 			const boost::filesystem::path cp = ( *i );
-			m_configPaths.emplace_back(cp);  
+			std::unique_ptr<GuiImportRetroArchConfigEntry> entry(new GuiImportRetroArchConfigEntry(cp));
+			AddEntry(std::move(entry));
 			
 		}
 		if (count == 0)
@@ -34,156 +46,10 @@ GuiImportRetroArchConfig::GuiImportRetroArchConfig(
 				"Close", [ this ] { delete this; }));
 		}
 	}
-
-	mMenu.setSize(Renderer::getScreenWidth() * 0.75f, mMenu.getSize().y());
-	mMenu.setPosition(( mSize.x() - mMenu.getSize().x() ) / 2, Renderer::getScreenHeight() * 0.15f);
-
-	m_nextPageButton = mMenu.addButton("Next Page", "Pages", [ this ] { LoadNextPages(1); });
-	m_nextBulkPageButton = mMenu.addButton("+10", "Pages", [ this ] { LoadNextPages(10); });
-
-	m_prevPageButton = mMenu.addButton("Prev", "Pages", [ this ] { LoadPrevPages(1); });
-	m_prevBulkPageButton = mMenu.addButton("-10", "Pages", [ this ] { LoadPrevPages(10); });
-
-	m_pageCountButton = mMenu.addButton(GetPageLabelText(), "Pages", [ this ] {  });
-
-	m_pageCountButton->setEnabled(false);
-
 	LoadPage(0u);
-}
-
-std::string GuiImportRetroArchConfig::GetPageLabelText() const
-{
-	return "Page " + std::to_string(m_currentPage+1) + "/" + std::to_string(GetLastPage()+1);
-}
-
-uint32_t GuiImportRetroArchConfig::GetLastPage() const
-{
-	return static_cast<uint32_t>(std::ceil(m_configPaths.size() / k_pageEntryCount));
-}
-
-bool GuiImportRetroArchConfig::IsLastPage() const
-{
-	return m_currentPage == GetLastPage();
-}
-
-bool GuiImportRetroArchConfig::IsFirstPage() const
-{
-	return m_currentPage == 0u;
-}
-
-void GuiImportRetroArchConfig::LoadPrevPages(uint32_t count)
-{
-	if (IsFirstPage()) return;
-	const uint32_t prevPage = ( count >= m_currentPage ) ? 0u : m_currentPage - count;
-	LoadPage(prevPage);
-}
-
-void GuiImportRetroArchConfig::LoadNextPages(uint32_t count)
-{
-	if (IsLastPage()) return;
-
-	const uint32_t nextPage = std::min(m_currentPage + count, GetLastPage());
-	LoadPage(nextPage);
-}
-
-void GuiImportRetroArchConfig::LoadPage(uint32_t page)
-{
-	mMenu.ClearRows();
-
-	uint32_t rowIndex = std::min(page*k_pageEntryCount, m_configPaths.size() - k_pageEntryCount);
-	uint32_t rowCount = std::min(rowIndex + k_pageEntryCount, m_configPaths.size());
-	for (; rowIndex < rowCount; ++rowIndex)
-	{
-		InsertRow(m_configPaths[ rowIndex ]);
-	}
-	m_currentPage = page;
-	m_pageCountButton->setText(GetPageLabelText(), "Pages");
-	m_prevPageButton->setVisible(!IsFirstPage());
-	m_nextPageButton->setVisible(!IsLastPage());
-
-	m_nextBulkPageButton->setVisible(!IsLastPage());
-	m_prevBulkPageButton->setVisible(!IsFirstPage());
-}
-
-void GuiImportRetroArchConfig::InsertRow(boost::filesystem::path path)
-{
-	ComponentListRow row;
-	const std::string title = path.filename().generic_string();
-	row.addElement(std::make_shared<TextComponent>(mWindow, title, Font::get(FONT_SIZE_MEDIUM), 0x777777FF), true);
-	row.input_handler = std::bind(&GuiImportRetroArchConfig::OnRowSelected, this, std::placeholders::_1, std::placeholders::_2, path);
-	addRow(row);
 }
 
 GuiImportRetroArchConfig::~GuiImportRetroArchConfig()
 {
 
-}
-
-bool GuiImportRetroArchConfig::OnRowSelected(InputConfig* config, Input input, boost::filesystem::path configPath)
-{
-	if (config->isMappedTo("a", input) && input.value)
-	{
-		m_onConfigSelected(configPath);
-		delete this;
-		return true;
-	}
-	return false;
-}
-
-bool GuiImportRetroArchConfig::input(InputConfig* config, Input input)
-{
-	if (GuiOptionWindow::input(config, input))
-	{
-		return true;
-	}
-
-	if (config->isMappedTo("right", input) && input.value != 0)
-	{
-		LoadNextPages();
-		return true;
-	}
-	if (config->isMappedTo("left", input) && input.value != 0)
-	{
-		LoadPrevPages();
-		return true;
-	}
-
-	if (config->isMappedTo("PageUp", input) && input.value != 0)
-	{
-		LoadNextPages(10);
-		return true;
-	}
-	if (config->isMappedTo("PageDown", input) && input.value != 0)
-	{
-		LoadPrevPages(10);
-		return true;
-	}
-
-	if (config->isMappedTo("b", input) && input.value != 0)
-	{
-		delete this;
-		return true;
-	}
-
-	if (config->isMappedTo("select", input) &&
-		input.value != 0)
-	{
-		// close everything
-		Window* window = mWindow;
-		while (window->peekGui() && window->peekGui() != ViewController::get())
-			delete window->peekGui();
-		return true;
-	}
-
-	return GuiComponent::input(config, input);
-}
-
-std::vector<HelpPrompt> GuiImportRetroArchConfig::getHelpPrompts()
-{
-	std::vector<HelpPrompt> prompts = mMenu.getHelpPrompts();
-
-	prompts.push_back(HelpPrompt("b", "back"));
-	prompts.push_back(HelpPrompt("select", "close"));
-
-	return prompts;
 }
--- es-app/src/guis/GuiImportRetroArchConfig.h
+++ es-app/src/guis/GuiImportRetroArchConfig.h
@@ -1,8 +1,8 @@
 #pragma  once
-#include "guis/GuiOptionWindow.h"
 #include "components/ButtonComponent.h"
+#include "GuiPagedListView.h"
 
-class GuiImportRetroArchConfig : public GuiOptionWindow
+class GuiImportRetroArchConfig : public GuiPagedListView
 {
 public:
 	using CallbackT = std::function<void(boost::filesystem::path)>;
@@ -10,29 +10,5 @@ public:
 	virtual ~GuiImportRetroArchConfig();
 
 private:
-	std::string GetPageLabelText() const;
-	uint32_t GetLastPage() const;
-	bool IsLastPage() const;
-	bool IsFirstPage() const;
-	void LoadNextPages(uint32_t count = 1);
-	void LoadPrevPages(uint32_t count = 1);
-	void LoadPage(uint32_t page);
-	void InsertRow(boost::filesystem::path path);
-	bool OnRowSelected(InputConfig* config, Input input,  boost::filesystem::path);
-	
-	bool input(InputConfig* config, Input input) override;
-
-	std::vector<HelpPrompt> getHelpPrompts() override;
-	boost::filesystem::path m_configFolder;
 	CallbackT m_onConfigSelected;
-	std::vector<boost::filesystem::path> m_configPaths;
-	uint32_t m_currentPage;
-	static const uint32_t k_pageEntryCount;
-
-	std::shared_ptr<ButtonComponent> m_pageCountButton;
-	std::shared_ptr<ButtonComponent> m_nextPageButton;
-	std::shared_ptr<ButtonComponent> m_prevPageButton;
-	std::shared_ptr<ButtonComponent> m_nextBulkPageButton;
-	std::shared_ptr<ButtonComponent> m_prevBulkPageButton;
-
 };
