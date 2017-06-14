a77553460738ea0ec8ba3f791a23d1d913ac80d8
Alex Tinti <alextinti83@gmail.com>
2017-06-14 07:57:19 +0200

    + create new collection with "y"
    + and allow changing the current collection

--- es-app/src/guis/GuiGameCollections.cpp
+++ es-app/src/guis/GuiGameCollections.cpp
@@ -7,6 +7,7 @@
 #include "components/SwitchComponent.h"
 
 
+
 GuiGameCollections::GuiGameCollections(
 	Window* window, 
 	SystemData& systemData)
@@ -18,6 +19,9 @@ GuiGameCollections::GuiGameCollections(
 	{
 		InsertEntry(collection.first);
 	}
+
+	const std::string currentCollectionName = mSystemData.GetCurrentGameCollection()->GetName();
+	SetCurrent(currentCollectionName);
 }
 
 GuiGameCollections::~GuiGameCollections()
@@ -25,33 +29,72 @@ GuiGameCollections::~GuiGameCollections()
 
 }
 
-
-void GuiGameCollections::InsertEntry(const std::string& entryName)
+void GuiGameCollections::InsertEntry(const std::string& key)
 {
-	const std::string title = entryName;
-	ComponentListRow row;
-	row.addElement(std::make_shared<TextComponent>(mWindow, strToUpper(title), Font::get(FONT_SIZE_MEDIUM), 0x777777FF), true);
-	auto onOffSwitch = std::make_shared<SwitchComponent>(mWindow);
-	onOffSwitch->setState(true);
-	row.addElement(onOffSwitch, false, true);
+	GameCollectionEntry row;
+	row.key = key;
 
+	row.textComponent = std::make_shared<TextComponent>(mWindow, strToUpper(key), Font::get(FONT_SIZE_MEDIUM), 0x777777FF);
+	row.switchComponent = std::make_shared<SwitchComponent>(mWindow);
+	
+	row.switchComponent->setState(false);
+	row.switchComponent->setVisible(false);
+
+	row.addElement(row.textComponent, true);
+	row.addElement(row.switchComponent, false, true);
+
+	m_entries.emplace(key, row);
 	row.input_handler = std::bind(&GuiGameCollections::OnEntrySelected,
 		this,
 		std::placeholders::_1,
 		std::placeholders::_2,
-		entryName, onOffSwitch);
+		GetEntry(key));
+
 	addRow(row);
 }
 
-bool GuiGameCollections::OnEntrySelected(InputConfig* config, Input input, std::string entryname, std::shared_ptr<SwitchComponent> switchComp)
+
+bool GuiGameCollections::OnEntrySelected(InputConfig* config, Input input, 
+	GameCollectionEntry* selectedEntry)
 {
 	if (config->isMappedTo("a", input) && input.value)
 	{
-		const bool newState = !switchComp->getState();
-		switchComp->setVisible(newState);
-		switchComp->setState(newState);
-
+		SetCurrent(selectedEntry->key);
+		ViewController::get()->reloadGameListView(&mSystemData);
 		return true;
 	}
+	else if (config->isMappedTo("y", input) && input.value)
+	{
+		const std::size_t count = mSystemData.GetGameCollections().size();
+		const std::string name = "New Collection " + std::to_string(count);
+		mSystemData.NewGameCollection(name);
+		InsertEntry(name);
+	}
 	return false;
+}
+
+GameCollectionEntry* GuiGameCollections::GetEntry(const std::string key)
+{
+	auto it = m_entries.find(key);
+	if (it != m_entries.end())
+	{
+		return &it->second;
+	}
+	return nullptr;
+}
+
+void GuiGameCollections::SetCurrent(const std::string key)
+{
+	for (auto& pair : m_entries)
+	{
+		pair.second.switchComponent->setVisible(false);
+		pair.second.switchComponent->setState(false);
+	}
+	auto entry = GetEntry(key);
+	if (entry)
+	{
+		entry->switchComponent->setVisible(true);
+		entry->switchComponent->setState(true);
+		mSystemData.SetCurrentGameCollection(key);
+	}
 }
--- es-app/src/guis/GuiGameCollections.h
+++ es-app/src/guis/GuiGameCollections.h
@@ -1,7 +1,17 @@
 #pragma  once
 #include "GuiOptionWindow.h"
+#include "components/ComponentList.h"
+
 class SystemData;
 class SwitchComponent;
+
+class GameCollectionEntry : public ComponentListRow
+{
+public:
+	std::string key;
+	std::shared_ptr<TextComponent> textComponent;
+	std::shared_ptr<SwitchComponent> switchComponent;
+};
 
 class GuiGameCollections : public GuiOptionWindow
 {
@@ -11,8 +21,11 @@ public:
 
 	void InsertEntry(const std::string& entryName);
 private:
-	bool OnEntrySelected(InputConfig* config, Input input, std::string entryname, std::shared_ptr<SwitchComponent> switchComp);
+	bool OnEntrySelected(InputConfig* config, Input input, GameCollectionEntry* selectedEntry);
+	GameCollectionEntry* GetEntry(const std::string key);
+	void SetCurrent(const std::string key);
 
 	Window* mWindow;
 	SystemData& mSystemData;
+	std::map<const std::string, GameCollectionEntry> m_entries;
 };
